570c253b15c52b158d376a71e9b947d91443b9c5
ODD-Materialise <oddmaterialise@yahoo.fr>
2016-04-17 15:25:22 -0400

    Pleins de fichiers test pour implémentation switch/tab

--- app/src/main/java/amaury/test1/mainTest.java
+++ app/src/main/java/amaury/test1/mainTest.java
@@ -1,7 +1,12 @@
 package amaury.test1;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class mainTest extends AppCompatActivity {
 
